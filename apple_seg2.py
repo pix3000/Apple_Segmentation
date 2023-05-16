c1b3a270816a98d4b48d970c1e56a31d37567d45
H.J <51011169+pix3000@users.noreply.github.com>
2023-05-16 10:20:49 +0900

    Create apple_seg2.py

---
+++ apple_seg2.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+img_name = "test/A08F.JPG"
+
+image = cv2.imread(img_name)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# Red color range definition
+lower_red = np.array([0, 50, 50])
+upper_red = np.array([10, 255, 255])
+lower_red2 = np.array([170, 50, 50])
+upper_red2 = np.array([180, 255, 255])
+
+# Convert the image to HSV
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# Create masks for red regions
+mask1 = cv2.inRange(hsv, lower_red, upper_red)
+mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+mask = cv2.bitwise_or(mask1, mask2)
+
+# Apply the mask to extract only the red regions
+red_apple = cv2.bitwise_and(image, image, mask=mask)
+
+# Count the number of objects
+num_labels, labeled_image = cv2.connectedComponents(mask)
+
+# Subtract 1 from the total count to exclude the background label
+num_objects = int(num_labels /45) 
+print("Number of objects:", num_objects)
+
+cv2.imwrite(f"result/{img_name}", red_apple)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
