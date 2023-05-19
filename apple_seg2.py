b3f311dfb18445abe2a6d003637d1a6b09c97626
HJ Gwak <51011169+pix3000@users.noreply.github.com>
2023-05-19 14:35:23 +0900

    Update apple_seg2.py

--- apple_seg2.py
+++ apple_seg2.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
+import argparse
 
-img_path = "test/A08F.JPG"
-img_name = img_path.split("/")[1]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-img","--img_path", type=str, help="Path to the image")
+args = parser.parse_args()
+
+img_path = args.img_path
+img_name = img_path.split("/")[-1]
 
 image = cv2.imread(img_path)
 
@@ -23,16 +29,22 @@ mask1 = cv2.inRange(hsv, lower_red, upper_red)
 mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
 mask = cv2.bitwise_or(mask1, mask2)
 
+# Apply morphological operations to improve mask
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13, 13))
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
 # Apply the mask to extract only the red regions
 red_apple = cv2.bitwise_and(image, image, mask=mask)
 
 cv2.imwrite(f"result/{img_name}", red_apple)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # Count the number of objects
 num_labels, labeled_image = cv2.connectedComponents(mask)
 
 # Subtract 1 from the total count to exclude the background label
-num_objects = int(num_labels /45) 
+num_objects = int(num_labels)
 print(f"{img_name}:", num_objects)
+
